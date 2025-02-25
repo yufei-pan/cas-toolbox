e95bb7d49a34453585c8fe2b86ee2423aa2e12db
Yufei Pan <pan@zopyr.us>
2025-02-24 17:12:29 -0800

    Bump version to 2025.9.1 and update dependencies; add script to retrieve CAS versions

---
+++ getCASVersions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python3 ../hpcp/hpcp.py -V
+python3 ../multiCMD/multiCMD.py -V
+python3 ../multiSSH3/multiSSH3.py -V
+python3 ../simple-iotest/iotest.py -V
+python3 ../statbtrfs/statbtrfs.py -V
+python3 ../Tee_Logger/src/Tee_Logger.py -V
+python3 ../TSVZ/TSVZ.py -V
