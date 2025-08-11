3376d4bb3a885f549078edd49f44a83590ef6b12
Yufei Pan <pan@zopyr.us>
2025-08-11 14:48:39 -0700

    change the tee loger path

--- getCASVersions.sh
+++ getCASVersions.sh
@@ -4,5 +4,5 @@ python3 ../multiCMD/multiCMD.py -V
 python3 ../multiSSH3/multiSSH3.py -V
 python3 ../simple-iotest/iotest.py -V
 python3 ../statbtrfs/statbtrfs.py -V
-python3 ../Tee_Logger/src/Tee_Logger.py -V
+python3 ../tee-logger/src/Tee_Logger.py -V
 python3 ../TSVZ/TSVZ.py -V
