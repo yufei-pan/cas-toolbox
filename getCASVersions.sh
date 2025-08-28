95f55194c8e7b564e5c8dd537671220f846807a5
Yufei Pan <pan@zopyr.us>
2025-08-27 20:16:54 -0700

    update new statblk program to cas toolbox

--- getCASVersions.sh
+++ getCASVersions.sh
@@ -3,6 +3,7 @@ python3 ../hpcp/hpcp.py -V
 python3 ../multiCMD/multiCMD.py -V
 python3 ../multiSSH3/multiSSH3.py -V
 python3 ../simple-iotest/iotest.py -V
-python3 ../statbtrfs/statbtrfs.py -V
+../statbtrfs/.venv/bin/python ../statbtrfs/statbtrfs.py -V
 python3 ../tee-logger/src/Tee_Logger.py -V
 python3 ../TSVZ/TSVZ.py -V
+python3 ../statblk/statblk.py -V
